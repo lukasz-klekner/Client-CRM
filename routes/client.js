c2ffb7e0c41703b41c3a2721c2d09faeb1f7835f
Łukasz Klekner <lukaszklekner@gmail.com>
2022-12-04 21:01:14 +0100

    feat: create crud's schema

--- routes/client.js
+++ routes/client.js
@@ -2,9 +2,22 @@ const express = require('express')
 
 const clientRouter = express.Router()
 
-clientRouter.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+clientRouter
+    .get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+    .get('/:id', (req, res) => {
+        res.send('Hello World 2!')
+    })
+    .post('/', (req, res) => {
+        res.send('Hello World POST!')
+    })
+    .put('/:id', (req, res) => {
+        res.send('Hello World PUT!')
+    })
+    .delete('/:id', (req, res) => {
+        res.send('Hello World DELETE!')
+    })
 
 module.exports = {
     clientRouter
